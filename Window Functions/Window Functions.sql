dc037fc7bdebd431770481e909dd85ea3d355f56
Nawfel Hamdi <hamdii.nawfel@gmail.com>
2025-04-13 13:37:16 +0100

    change user email

--- Window Functions/Window Functions.sql
+++ Window Functions/Window Functions.sql
@@ -2,7 +2,7 @@
 -- Author:		Nawfel HAMDI
 -- Create date: 13/04/2025
 -- Description:	Window Functions
--- Link : https://www.youtube.com/watch?v=Wvg4PjbMTO8
+-- Link : https://datawithbaraa.substack.com/p/your-guide-to-sql-window-functions - https://www.youtube.com/watch?v=Wvg4PjbMTO8
 -- =============================================
 
 SELECT * FROM EmpDeptWiseSalary
