b080aaabdbc0e8d47c9a04e2f81a19d50bf5fe21
Nawfel Hamdi <hamdii.nawfel@gmail.com>
2025-04-13 16:48:07 +0100

    add new section

---
+++ Window Functions/2-Aggregate Window Functions.sql
@@ -0,0 +1,7 @@
+-- =============================================
+-- Author:		Nawfel HAMDI
+-- Create date: 13/04/2025
+-- Description:	Window Functions
+-- Link : https://datawithbaraa.substack.com/p/your-guide-to-sql-window-functions - https://www.youtube.com/watch?v=Wvg4PjbMTO8
+-- =============================================
+
